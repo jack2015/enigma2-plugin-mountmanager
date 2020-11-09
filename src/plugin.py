a79a63c5a69a3dec85b13487c461879e4132d274
Dimitrij <Dima-73@inbox.lv>
2020-11-09 12:34:54 +0200

    add duo4kse

--- src/plugin.py
+++ src/plugin.py
@@ -188,7 +188,7 @@ class DevicesMountPanel(Screen, ConfigListScreen):
 				continue
 			device = parts[3]
 			mmc = False
-			if MODEL_NAME in ('sf8008', 'sf5008', 'et13000', 'et11000','et1x000', 'duo4k', 'uno4k', 'uno4kse', 'ultimo4k', 'solo4k', 'zero4k', 'hd51', 'hd52', 'dm820', 'dm7080', 'sf4008', 'dm900', 'dm920', 'gbquad4k', 'gbue4k', 'lunix3-4k', 'lunix-4k', 'vs1500', 'h7', '8100s', 'e4hd', 'gbmv200', 'multibox', 'h9combo', 'h10', 'v8plus', 'hd60', 'hd61') and search('mmcblk0p[1-9]',device):
+			if MODEL_NAME in ('sf8008', 'sf5008', 'et13000', 'et11000','et1x000', 'duo4k', 'duo4kse', 'uno4k', 'uno4kse', 'ultimo4k', 'solo4k', 'zero4k', 'hd51', 'hd52', 'dm820', 'dm7080', 'sf4008', 'dm900', 'dm920', 'gbquad4k', 'gbue4k', 'lunix3-4k', 'lunix-4k', 'vs1500', 'h7', '8100s', 'e4hd', 'gbmv200', 'multibox', 'h9combo', 'h10', 'v8plus', 'hd60', 'hd61') and search('mmcblk0p[1-9]',device):
 				continue
 			if device and search('mmcblk[0-9]p[1-9]',device):
 				mmc = True
@@ -805,7 +805,7 @@ class DeviceMountPanelConf(Screen, ConfigListScreen):
 				continue
 			device = parts[3]
 			mmc = False
-			if MODEL_NAME in ('sf8008', 'sf5008', 'et13000', 'et11000', 'et1x000', 'duo4k', 'uno4k', 'uno4kse', 'ultimo4k', 'solo4k', 'zero4k', 'hd51', 'hd52', 'dm820', 'dm7080', 'sf4008', 'dm900', 'dm920', 'gbquad4k', 'gbue4k', 'lunix3-4k', 'lunix-4k', 'vs1500', 'h7', '8100s', 'e4hd', 'gbmv200', 'multibox', 'h9combo', 'h10', 'v8plus', 'hd60', 'hd61') and search('mmcblk0p[1-9]',device):
+			if MODEL_NAME in ('sf8008', 'sf5008', 'et13000', 'et11000', 'et1x000', 'duo4k', 'duo4kse', 'uno4k', 'uno4kse', 'ultimo4k', 'solo4k', 'zero4k', 'hd51', 'hd52', 'dm820', 'dm7080', 'sf4008', 'dm900', 'dm920', 'gbquad4k', 'gbue4k', 'lunix3-4k', 'lunix-4k', 'vs1500', 'h7', '8100s', 'e4hd', 'gbmv200', 'multibox', 'h9combo', 'h10', 'v8plus', 'hd60', 'hd61') and search('mmcblk0p[1-9]',device):
 				continue
 			if device and search('mmcblk[0-9]p[1-9]',device):
 				mmc = True
